c0a5f8d543004a5c682ae54021b90ea89e2efb36
Boban Acimovic <boban.acimovic@gmail.com>
2021-12-15 13:35:32 +0100

    fix job

--- job.go
+++ job.go
@@ -9,13 +9,13 @@ type InMsg struct{}
 
 type OutMsg struct{}
 
-var _ service.Job[*InMsg, *OutMsg] = (*job)(nil)
+var _ service.Job[*InMsg, *OutMsg] = (*Job)(nil)
 
-type job struct {
+type Job struct {
 	log lax.Logger
 }
 
-func (j *job) Execute(msg *InMsg) *OutMsg {
+func (j *Job) Execute(msg *InMsg) *OutMsg {
 	// do something with msg
 	_ = msg
 
